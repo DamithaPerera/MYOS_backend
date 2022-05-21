9260546b5ed5c0034cfc65e02ec2bbadd52b3cc4
Damitha Perera <damitha.perera@keeneye.solutions>
2022-05-21 22:55:10 +0530

    order validation

--- modules/orders/orders.router.ts
+++ modules/orders/orders.router.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 
 import {createOrderController} from "./orders.controller";
+import {taskBody} from "./orders.validation";
 
 
 const routes = Router();
 
 
-routes.post('/', createOrderController);
+routes.post('/', taskBody, createOrderController);
 
 
 export default routes;
---
+++ modules/orders/orders.validation.ts
@@ -0,0 +1,37 @@
+import Joi from "joi";
+import {NextFunction, Request, Response} from 'express';
+import {commonResponse} from "../../util/response";
+
+/**
+ * orderBody validation
+ * @param req
+ * @param res
+ * @param next
+ */
+let response;
+export const taskBody = (req: Request, res: Response, next: NextFunction) => {
+
+    const schema = Joi.object({
+        order: Joi.array()
+            .items({
+                quantity: Joi.number().integer().min(1)
+                    .required(),
+                productId: Joi.number().integer().min(1)
+                    .required()
+            })
+    });
+
+    const result = schema.validate(req.body);
+    if (result.error) {
+        console.log(result.error.message);
+        response = commonResponse(
+            false,
+            null,
+            result.error.message,
+            result.error
+        );
+        res.status(422).send(response);
+    } else {
+        next()
+    }
+};
